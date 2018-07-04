16ddfda13cf65a47c3ac2430b48f44c493704438
Ken Lowrie <klowrie@gmail.com>
2018-07-04 07:10:52 -0500

    Give it a unique name for PyPI
    pylib is already taken, so renamed to pylib_kenl380

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='pylib',
+setup(name='pylib_kenl380',
       version='0.8.8',
       description='Python shared library',
       url='https://github.com/kenlowrie/pylib',
